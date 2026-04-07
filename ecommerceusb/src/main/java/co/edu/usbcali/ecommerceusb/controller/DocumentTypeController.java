beb8b3a9af9559ce67d257560544314f469f3b30
samechebo <75792137+samechebo@users.noreply.github.com>
2026-04-06 20:56:28 -0500

    Cambios nuevos

--- ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/controller/DocumentTypeController.java
+++ ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/controller/DocumentTypeController.java
@@ -1,4 +1,44 @@
 package co.edu.usbcali.ecommerceusb.controller;
 
+import co.edu.usbcali.ecommerceusb.dto.DocumentTypeResponse;
+import co.edu.usbcali.ecommerceusb.mapper.DocumentTypeMapper;
+import co.edu.usbcali.ecommerceusb.model.DocumentType;
+import co.edu.usbcali.ecommerceusb.repository.DocumentTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/document-type")
 public class DocumentTypeController {
+
+    @Autowired
+    private DocumentTypeRepository documentTypeRepository;
+
+    @GetMapping("/all")
+    public List<DocumentTypeResponse> getAll() {
+
+        return DocumentTypeMapper.modelToDocumentTypeResponseList(
+                documentTypeRepository.findAll()
+        );
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DocumentTypeResponse> getById(@PathVariable Integer id){
+        DocumentType documentType= documentTypeRepository.getReferenceById(id);
+        DocumentTypeResponse documentTypeResponse=
+                DocumentTypeMapper.modelToDocumentTypeResponse(documentType);
+        return new ResponseEntity<>(
+                documentTypeResponse,
+                HttpStatus.OK
+        );
+    }
+
 }
---
+++ ecommerceusb/src/main/java/co/edu/usbcali/ecommerceusb/mapper/DocumentTypeMapper.java
@@ -0,0 +1,30 @@
+package co.edu.usbcali.ecommerceusb.mapper;
+
+import co.edu.usbcali.ecommerceusb.dto.DocumentTypeResponse;
+import co.edu.usbcali.ecommerceusb.model.DocumentType;
+
+import java.util.List;
+
+public class DocumentTypeMapper {
+    public static DocumentTypeResponse modelToDocumentTypeResponse(DocumentType documentType){
+        return DocumentTypeResponse.builder()
+                .id(documentType.getId())
+                .code(documentType.getCode())
+                .name(documentType.getName())
+                .build();
+
+    }
+    public static List<DocumentTypeResponse>modelToDocumentTypeResponseList(List<DocumentType> documentTypes) {
+        /*List<DocumentTypeResponse>documentTypeResponseList = null;
+        for(DocumentType documentType : documentTypes){
+            DocumentTypeResponse documentTypeResponse = modelToDocumentTypeResponse(documentType);
+            documentTypeResponseList.add(documentTypeResponse);
+        }
+
+        return documentTypeResponseList*/
+        return documentTypes.stream().map(DocumentTypeMapper::modelToDocumentTypeResponse).toList();
+    }
+
+
+
+}
